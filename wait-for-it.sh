c82863c4878f61645eff512c9f3e2c853317f23f
Subhan Nizar <subhannizar25@gmail.com>
2019-10-09 20:35:19 +0800

    Fixing bug

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -9,12 +9,14 @@ export FLASK_DEBUG=1
 export DATABASE_URI=postgresql+psycopg2://hwpjqrrycaadqr:cca5058487d93272900e4f468902e0cec78ba7915e983737752a3e30241ea6fc@ec2-54-235-114-242.compute-1.amazonaws.com/d74e9hcikf56dg
 export BACKEND_BASE_URL=api
 
-until test -z "$(ps -A | grep mysqld)"; do
-    >&2 echo "MySQL is unavailable - sleeping"
-    sleep 1
-done
+pip install bcrypt
 
->&2 echo "MySQL is up - executing command"
+# until test -z "$(ps -A | grep mysqld)"; do
+#     >&2 echo "MySQL is unavailable - sleeping"
+#     sleep 1
+# done
+
+# >&2 echo "MySQL is up - executing command"
 
 flask db init
 flask db migrate
