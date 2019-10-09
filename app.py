a6e70cb720db67c84520dba2f817fc8602ee89dd
Subhan Nizar <subhannizar25@gmail.com>
2019-10-09 21:36:37 +0800

    Binding gunicorn

--- app.py
+++ app.py
@@ -2,8 +2,4 @@ from side_service import app as instance
 
 
 if __name__ == "__main__":
-    instance.run(
-        instance.config["HOST"],
-        instance.config["PORT"],
-        debug=instance.config["DEBUG"]
-    )
+    instance.run()
